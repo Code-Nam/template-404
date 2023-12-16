d7643e2fd170a0f1803c24818345344687907f5d
AnnamTRAN <annampierretran@gmail.com>
2023-12-16 23:29:39 +0100

    🚧 - WORKING on template 3

---
+++ template-3/script.js
@@ -0,0 +1,27 @@
+const title = document.querySelector('.title');
+const logo = document.querySelector('.logo');
+const textWhopper = document.querySelector('.text-whopper');
+const textContent = document.querySelector('.text-content');
+
+// setTimeout(() => {
+//   title.classList.add('fade-out');
+//   setTimeout(() => {
+//     logo.classList.remove('hide');
+//   }, 1000);
+// }, 2000);
+
+console.log(textWhopper.scrollTop)
+
+// textContent.style.transform = "rotateX(20deg) translateY(100%)";
+
+// textWhopper.addEventListener('scroll' , () =>{
+//   const scrollValue = textWhopper.scrollY;
+
+//   textContent_translateY = -100 + (getPercentage(0, textContentBottom));
+
+//   if(textContent_translateY > 100){
+//     textContent_translateY = 100;
+//   }
+
+//   textContent.style.transform = `rotateX(20deg) translateY(${textContent_translateY}%)`;
+// })
