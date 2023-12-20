4940c79d646999bcec9314eb85d841517335665e
AnnamTRAN <114738120+AnnamTRAN@users.noreply.github.com>
2023-12-20 20:48:36 +0100

    :beers: - FINISHED (not scrollable) template 3

--- template-3/script.js
+++ template-3/script.js
@@ -1,27 +1,32 @@
 const title = document.querySelector('.title');
 const logo = document.querySelector('.logo');
-const textWhopper = document.querySelector('.text-whopper');
 const textContent = document.querySelector('.text-content');
+const theme = new Audio('../assets/audio/theme.mp3');
+theme.volume = 0.5;
 
-// setTimeout(() => {
-//   title.classList.add('fade-out');
-//   setTimeout(() => {
-//     logo.classList.remove('hide');
-//   }, 1000);
-// }, 2000);
+let textScroll = 80;
 
-console.log(textWhopper.scrollTop)
+const loopScroll = () => {
+  setTimeout(() => {
+    textContent.style.transform = `rotateX(15deg) translateY(${textScroll}%)`;
+    textScroll -= 0.35;
+    if (textScroll > -100) {
+      loopScroll();
+    } else {
+      location.reload();
+    }
+  }, 100);
+}
 
-// textContent.style.transform = "rotateX(20deg) translateY(100%)";
+setTimeout(() => {
+  title.classList.add('fade-out');
+  setTimeout(() => {
+    theme.play();
+    logo.classList.remove('hide');
+      setTimeout(() => { // Start Loop Scroll
+        loopScroll();
+      }, 1000);
+  }, 1000);
+}, 2000);
 
-// textWhopper.addEventListener('scroll' , () =>{
-//   const scrollValue = textWhopper.scrollY;
 
-//   textContent_translateY = -100 + (getPercentage(0, textContentBottom));
-
-//   if(textContent_translateY > 100){
-//     textContent_translateY = 100;
-//   }
-
-//   textContent.style.transform = `rotateX(20deg) translateY(${textContent_translateY}%)`;
-// })
